6adeb4d7561be35dbcb4bbcd2a2db2f28eaa6971
Mohammad-Al-Sharef <www.malsharef57@gmail.com>
2024-10-08 19:24:02 +0300

    Add project files.

---
+++ task3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            //string[] ARR = { "1", "7", "9", "45" };
+            //string[] arr2 = { "Str", "alex", "moh" };
+            //string[] arr3 = { "the", "fox", "over", "lazy", "dog" };
+
+            //Q2
+            //"Banana" is 1
+            // "Tomato" is 0
+
+            //Q3
+            //string[] favoriteFood = { "Apple", "banana", "strawberry", "watermelon", "tomato" };
+            //string[] favoriteSport = { "Swimming", "shooting", "horse riding" };
+            //string[] favoriteMovie = { "Othman", "alrisala", "yousef", "omar almoktar" };
+
+            //Console.WriteLine("Favorite Food:");
+
+            //foreach (string food in favoriteFood)
+            //{
+            //    Console.WriteLine(food);
+            //}
+
+
+            //Console.WriteLine("favorite Sport:");
+
+            //foreach (string food in favoriteSport)
+            //{
+            //    Console.WriteLine(food);
+            //}
+
+
+            //Console.WriteLine("favorite Movie :");
+
+            //foreach (string food in favoriteMovie)
+            //{
+            //    Console.WriteLine(food);
+            //}
+
+
+            //Q4
+            //Console.Write("Please enter 3 numbers separated by comma: ");
+            //string input = Console.ReadLine();
+
+
+            //string[] numbers = input.Split(',');
+
+
+            //int number1=Convert.ToInt32(numbers[0]);
+            //int number2 = Convert.ToInt32(numbers[1]);
+            //int number3 = Convert.ToInt32(numbers[2]);
+            //int sum = number1 + number2 + number3;
+
+
+            //Console.WriteLine(" sum : " + sum);
+
+
+            //Q5
+            //int sum = 0;
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i);
+
+            //        sum += i;
+
+            //    }
+            //}
+            //Console.WriteLine("sum : " + sum);
+
+            //Q6 
+
+            //int rows = 3;
+
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int space = 1; space <= rows - i; space++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //Q7
+            //int rows = 4;
+            //int number = 1; 
+
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int space = 1; space <= rows - i; space++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(number + " ");
+            //        number++; 
+            //    }
+
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
